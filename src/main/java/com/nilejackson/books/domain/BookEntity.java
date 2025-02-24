ce75ad4e89962b1442ab0d201f104995a516270e
Nile Jackson <nilejack@udel.edu>
2025-02-24 09:57:16 -0500

    Feat Adds: Setting up Book objects

---
+++ src/main/java/com/nilejackson/books/domain/BookEntity.java
@@ -0,0 +1,27 @@
+package com.nilejackson.books.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data //Calling Lombok to generate boilerplate 
+@AllArgsConstructor
+@NoArgsConstructor //Added this constructor generator because SpringBoot actually does look for a no args constructor/ JPA Hibernate needs this for entity instances when loading from db--JSON also uses this to instantiate objects during serialization 
+@Builder //establishes reliable pattern for building objects
+@Entity
+@Table(name = "books") // Uses Standard Java Enterprise Edition API for JPA 
+public class BookEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String author; 
+    private String isbn;
+    private String title; 
+    private String genre;
+}
