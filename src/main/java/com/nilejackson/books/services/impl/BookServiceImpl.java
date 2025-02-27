9165f5d428cd9e0dcbfb6ed58fdba254347aee75
Nile Jackson <nilejack@udel.edu>
2025-02-27 00:25:18 -0500

    Feat Adds: Methods for BookService layer implementation

--- src/main/java/com/nilejackson/books/services/impl/BookServiceImpl.java
+++ src/main/java/com/nilejackson/books/services/impl/BookServiceImpl.java
@@ -1,6 +1,7 @@
 package com.nilejackson.books.services.impl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import com.nilejackson.books.domain.BookEntity;
 import com.nilejackson.books.domain.Book;
 import com.nilejackson.books.repositories.BookRepository;
 import com.nilejackson.books.services.BookService;
@@ -11,14 +12,37 @@ public class BookServiceImpl implements BookService {
 
     private final BookRepository bookRepository;
 
+    // Constructor injection lets Spring automatically wire up our repository
+    // This way we don't have to create the repository object ourselves
+    @Autowired //Here for dependecy injection
     public BookServiceImpl(final BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
 
     @Override
     public Book create(final Book book) {
-       
-        return null;
+         // Step 1: Convert the incoming Book (API/domain model) to a BookEntity (database model)
+        // Think of this as translating from "web language" to "database language"
+       final BookEntity bookEntity = bookToBookEntity(book); 
+       final BookEntity savedBookEntity = bookRepository.save(bookEntity);
+       return bookEntityToBook(savedBookEntity);
     }
+
+    private BookEntity bookToBookEntity(Book book) {
+        return BookEntity.builder()
+        .isbn(book.getIsbn())
+        .title(book.getTitle())
+        .author(book.getAuthor())
+        .build();
+    }
+
+    private Book bookEntityToBook(BookEntity bookEntity) {
+        return Book.builder()
+        .isbn(bookEntity.getIsbn())
+        .title(bookEntity.getTitle())
+        .author(bookEntity.getAuthor())
+        .build();
+    }
+
     
 }
