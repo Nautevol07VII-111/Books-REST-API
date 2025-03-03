2d06ef65c870fdec250c2215dde02ade49833957
Nile Jackson <nilejack@udel.edu>
2025-03-03 16:44:13 -0500

    Feat Adds: Comments for study, implemented proper response handling in BookController createBook method. Added the correct HTTP response structure for best REST practices and added a partially correct return type(still need to add the saved book entity)

--- src/main/java/com/nilejackson/books/controllers/BookController.java
+++ src/main/java/com/nilejackson/books/controllers/BookController.java
@@ -1,6 +1,7 @@
 package com.nilejackson.books.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -15,17 +16,18 @@ public class BookController {
     private final BookService bookService;
     
     @Autowired //This AutoWired Annotation gives us the ability to automatically add dependencies as needed instead of having to configure them manually 
+    //To elaborate on the comment above, Spring to inject a dependency, in the case of My library API, Spring would inject something like BookServiceImpl whenever necessary-> This is a good example of loose coupling (the controller here only depends on the interface)
     public BookController(final BookService bookService) {
         this.bookService = bookService;
     }
     
 
     //Below we utilize something called a ResponseEntity which is an object from the Spring library that allows us to control things like response codes 
-    //The RequestBody Annotation tells Spring that when  it gets a request to this end to return the object returned at said endpoint 
+    //The @RequestBody annotation tells Spring to deserialize the request body into the annotated parameter. Ex: In this API incoming JSON in the request body is converted into book objects. 
     //@PutMapping maps HTTP PUT requests to this specific method
     @PutMapping(path = "/book/{isbn}")
     public ResponseEntity<Book> createBook(@RequestBody final Book book) {
-            return null;
+            return new ResponseEntity<>(null,HttpStatus.CREATED);
         
     }
 
