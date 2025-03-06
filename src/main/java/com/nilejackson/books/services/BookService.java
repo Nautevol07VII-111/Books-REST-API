8adb2a220c405e1038aed2f0064e1d5a6911f3b0
Nile Jackson <nilejack@udel.edu>
2025-03-06 17:30:32 -0500

    feat(service): implement book lookup by ID with Optional return type

--- src/main/java/com/nilejackson/books/services/BookService.java
+++ src/main/java/com/nilejackson/books/services/BookService.java
@@ -1,8 +1,13 @@
 package com.nilejackson.books.services;
 
+import java.util.Optional;
+
 import com.nilejackson.books.domain.Book;
 
 public interface BookService {
    
     Book createBook(Book book);
+    
+    
+    public Optional<Book> findById(Long Id);
 }
