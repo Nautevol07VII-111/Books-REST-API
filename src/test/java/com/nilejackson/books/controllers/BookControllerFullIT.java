f04474aa2d0912c0b1e5a797369108d25d1dafd5
Nile Jackson <nilejack@udel.edu>
2025-03-08 15:33:39 -0500

    fix: removed pointless comment

--- src/test/java/com/nilejackson/books/controllers/BookControllerFullIT.java
+++ src/test/java/com/nilejackson/books/controllers/BookControllerFullIT.java
@@ -22,7 +22,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK) // No server started
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK) 
 @AutoConfigureMockMvc
 public class BookControllerFullIT {
 
