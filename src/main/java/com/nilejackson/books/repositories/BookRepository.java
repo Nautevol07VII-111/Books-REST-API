11aa40f5a1c1a1620e676fcca77d259b246f20a5
Nile Jackson <nilejack@udel.edu>
2025-03-23 13:54:32 -0400

    add(bookRepo): added query method to find specific titles regardless of case

--- src/main/java/com/nilejackson/books/repositories/BookRepository.java
+++ src/main/java/com/nilejackson/books/repositories/BookRepository.java
@@ -1,5 +1,7 @@
 package com.nilejackson.books.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,5 @@ import com.nilejackson.books.domain.BookEntity;
 //essentially this is a class that allows for CRUD actions to take place! Right click the JpaRepository annotation and select implementation to see what's going on under the hood.
 @Repository //This allows us to utilize Spring's implemetation(a bunch of methods/interfaces that actually extend other interfaces that access even more methods) of the book repo which saves time and effort on our end
 public interface BookRepository extends JpaRepository<BookEntity, Long>{
-    
+    List<BookEntity> findByTitleIgnoreCase(String title); 
 }
