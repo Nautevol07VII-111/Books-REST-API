993e557b4d7a772db0f24cfaa55e6d953a78c94b
Nile Jackson <nilejack@udel.edu>
2025-03-27 18:56:40 -0400

    feat(pom.xml/test): explicit junit 4 config for EntityConfigurationTest.java setUp to ensure there are no issues or confusion during build processes

---
+++ src/test/java/com/nilejackson/books/EntityConfigurationTest.java
@@ -0,0 +1,24 @@
+package com.nilejackson.books;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import jakarta.persistence.EntityManager;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class EntityConfigurationTest {
+    @Autowired
+    private EntityManager entityManager;
+
+    @org.junit.jupiter.api.Test 
+    public void contextLoads() {
+        assertNotNull(entityManager);
+    }
+}
