ea254ebb4b033888723bcd0725782073947a7d89
Nile Jackson <nilejack@udel.edu>
2025-03-27 17:28:25 -0400

    fix(domain): removed unnecessary @table annotation and import

--- src/main/java/com/nilejackson/books/domain/Book.java
+++ src/main/java/com/nilejackson/books/domain/Book.java
@@ -2,7 +2,6 @@ package com.nilejackson.books.domain;
 
 import java.util.List;
 
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,7 +12,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 
-@Table(name = "books")
 public class Book {
     
     private Long id;
