807eca14fb32b9c32b574183ef48fcc1fd79ba0a
Nile Jackson <nilejack@udel.edu>
2025-03-27 21:11:16 -0400

    fix(test): passing entity configuration tests to ensure issues have been fixed

--- src/test/java/com/nilejackson/books/EntityConfigurationTest.java
+++ src/test/java/com/nilejackson/books/EntityConfigurationTest.java
@@ -2,10 +2,14 @@ package com.nilejackson.books;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.nilejackson.books.domain.BookEntity;
+import com.nilejackson.books.domain.UserEntity;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 
@@ -17,8 +21,20 @@ public class EntityConfigurationTest {
     @Autowired
     private EntityManager entityManager;
 
-    @org.junit.jupiter.api.Test 
+    @Test
     public void contextLoads() {
         assertNotNull(entityManager);
     }
+
+    @Test
+    public void cantFindBookEntityMetadata() {
+        assertNotNull(entityManager.getMetamodel().entity(BookEntity.class));
+    }
+
+    @Test 
+    public void cantFindUserEntityMetadata() {
+        assertNotNull(entityManager.getMetamodel().entity(UserEntity.class));
+    }
+
+   
 }
